9d1c2ef9cff812bc5558fa9dd3f90f6c128c6e23
irfandocker <irfan.gt11@gmail.com>
2021-09-22 14:08:46 -0400

    Initial commit

---
+++ src/test/java/com/junitTest/smokeTest/NumberTest.java
@@ -0,0 +1,17 @@
+package com.junitTest.smokeTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumberTest {
+
+	@Test
+	public void twoPlusTwoEqualfour() {
+		assertEquals("2+2=4",2+2,4);
+	}
+	
+	
+
+	
+}
